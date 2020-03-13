e83e561d44d545c3c95b29e9d74d393bc50456d5
zwyu123 <zwyu2017@163.com>
2020-03-13 23:17:49 +0800

    Initial commit

---
+++ src/main/java/com/imooc/MmallSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmallSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmallSpringbootApplication.class, args);
+	}
+
+}
